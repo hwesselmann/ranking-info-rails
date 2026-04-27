a05f86ec7098502061af9ea041366c9d9598b7b4
Hauke Wesselmann <hauke@h-dawg.de>
2026-04-27 19:58:30 +0200

    update to ruby 4 and rails 8

---
+++ app/javascript/application.js
@@ -0,0 +1,25 @@
+import Rails from "@rails/ujs"
+import * as ActiveStorage from "@rails/activestorage"
+import Chartkick from "chartkick"
+import Highcharts from "highcharts"
+import "./custom/cookie-banner"
+
+Rails.start()
+ActiveStorage.start()
+Chartkick.use(Highcharts)
+window.Chartkick = Chartkick
+
+document.addEventListener('DOMContentLoaded', () => {
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  if ($navbarBurgers.length > 0) {
+    $navbarBurgers.forEach(el => {
+      el.addEventListener('click', () => {
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+      });
+    });
+  }
+});
