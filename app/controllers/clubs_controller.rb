9f17709c09a3a7f5448b02f670726ac3e71f202b
Hauke Wesselmann <hauke@h-dawg.de>
2026-05-03 20:50:32 +0200

    add men and women rankings to clubs view and adapt sorting

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -10,8 +10,9 @@ class ClubsController < ApplicationController
       quarter = current_quarter
       player_clubs = find_all_clubs(quarter, params[:club])
       player_clubs.each do |c|
-        player_count = count_players_for_club(quarter, c.club)
-        clubs.push({ name: c.club, player_count: player_count })
+          youth_count = count_players_for_club(quarter, c.club, 'overall')
+        adult_count = count_players_for_club(quarter, c.club, %w[m00 w00])
+        clubs.push({ name: c.club, youth_count: youth_count, adult_count: adult_count })
       end
     end
     @clubs = clubs
@@ -19,9 +20,19 @@ class ClubsController < ApplicationController
 
   def show
     quarter = current_quarter
-    players = Ranking.where("date=? AND age_group='overall' AND LOWER(club)=LOWER(?)", quarter, params[:id])
-                     .order(:lastname)
-    player_ranking = fill_club_info(players, quarter)
+    youth = Ranking.where("date=? AND age_group='overall' AND LOWER(club)=LOWER(?)", quarter, params[:id])
+                   .order(:lastname)
+    player_ranking = {}
+    %w[m00 w00].each do |ag|
+      adults = Ranking.where(date: quarter, age_group: ag)
+                      .where('LOWER(club)=LOWER(?)', params[:id])
+                      .order(:ranking_position)
+      next if adults.empty?
+
+      label = ag == 'm00' ? 'Herren' : 'Damen'
+      player_ranking[label] = adults.map { |p| adult_player_data(p) }
+    end
+    player_ranking.merge!(fill_club_info(youth, quarter))
     @players = player_ranking
   end
 
@@ -42,8 +53,8 @@ class ClubsController < ApplicationController
            .distinct
   end
 
-  def count_players_for_club(quarter, club)
-    Ranking.where(date: quarter, age_group: 'overall', club: club).count
+  def count_players_for_club(quarter, club, age_group)
+    Ranking.where(date: quarter, age_group: age_group, club: club).count
   end
 
   def fill_club_info(players, quarter)
@@ -66,12 +77,18 @@ class ClubsController < ApplicationController
         u18_players.push(player_data)
       end
     end
-    player_ranking['U12'] = u12_players unless u12_players.empty?
-    player_ranking['U14'] = u14_players unless u14_players.empty?
-    player_ranking['U16'] = u16_players unless u16_players.empty?
-    player_ranking['U18'] = u18_players unless u18_players.empty?
+    player_ranking['U12'] = u12_players.sort_by { |p| p[:rank] } unless u12_players.empty?
+    player_ranking['U14'] = u14_players.sort_by { |p| p[:rank] } unless u14_players.empty?
+    player_ranking['U16'] = u16_players.sort_by { |p| p[:rank] } unless u16_players.empty?
+    player_ranking['U18'] = u18_players.sort_by { |p| p[:rank] } unless u18_players.empty?
 
     player_ranking
+  end
+
+  def adult_player_data(player)
+    { dtb_id: player.dtb_id, lastname: player.lastname,
+      firstname: player.firstname, score: player.score,
+      rank: player.ranking_position, age: player.age_group }
   end
 
   def fetch_basic_player_data(player, quarter)
