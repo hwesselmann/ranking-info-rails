a1a6bc76a49ca1a3baa7aea69a8b9b41e412629c
Hauke Wesselmann <hauke@h-dawg.de>
2026-05-05 14:40:21 +0200

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2022_07_20_150031) do
+ActiveRecord::Schema[8.1].define(version: 2022_07_20_150031) do
   create_table "rankings", force: :cascade do |t|
+    t.string "age_group"
+    t.boolean "age_group_ranking", default: false
+    t.string "club"
+    t.datetime "created_at", null: false
+    t.date "date"
     t.integer "dtb_id"
+    t.string "federation", limit: 50
     t.string "firstname", limit: 50
     t.string "lastname", limit: 50
-    t.string "federation", limit: 50
-    t.string "club"
     t.string "nationality", limit: 3
-    t.date "date"
-    t.string "age_group"
     t.integer "ranking_position"
     t.string "score"
-    t.boolean "age_group_ranking", default: false
-    t.boolean "yob_ranking", default: false
-    t.boolean "year_end_ranking", default: false
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "year_end_ranking", default: false
+    t.boolean "yob_ranking", default: false
     t.index ["age_group", "age_group_ranking", "yob_ranking", "year_end_ranking", "date"], name: "age_group_all_options"
     t.index ["age_group", "date"], name: "index_rankings_on_age_group_and_date"
     t.index ["club"], name: "index_rankings_on_club"
@@ -38,11 +38,11 @@ ActiveRecord::Schema[7.2].define(version: 2022_07_20_150031) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "email"
+    t.string "name"
     t.string "password_digest"
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
