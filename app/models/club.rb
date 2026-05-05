60cfe1427a336c44db2ef238fe5f238facb8cd66
Hauke Wesselmann <hauke@h-dawg.de>
2026-05-05 14:00:45 +0200

    refactor club functionality

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,13 +1,6 @@
 # frozen_string_literal: true
 
 #
-# Model for a club of a player on the ranking (this is a transient object)
+# Value object for a club of a player on the ranking.
 #
-class Club
-  attr_accessor :name, :federation
-
-  def initialize(club, federation)
-    @name = club
-    @federation = federation
-  end
-end
+Club = Struct.new(:name, :federation)
